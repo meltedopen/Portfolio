56196e7933c13eaf3f6f7af73b202721b7ea5b04
Isaac <isaac.chang5@gmail.com>
2019-09-28 00:02:16 -0700

    final push

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/static', express.static('public'));
+app.set('view engine', 'pug');
+
+//rename homeroute
+const mainRoutes = require('./routes');
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+  //change Not Found text
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+  console.log(err);
+  console.log(err.status);
+});
+
+app.listen(port, () => {
+  console.log(`The application is running at ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { data } = require('../data.json');
+const { projects } = data;
+
+router.get('/', (req, res) => {
+  res.render(`index`, { projects });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/projects/:id', (req, res) => {
+  res.locals.project = projects[parseInt(req.params.id)];
+  res.render('project');
+});
+
+module.exports = router;
